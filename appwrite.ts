1300fd9616716b3c279ca01a5d19c5b89cd34cf6
Osaretin Igbinobaro <mail@osaretin.dev>
2023-05-29 13:19:47 +0100

    feat: added the draggable and droppable columns

--- appwrite.ts
+++ appwrite.ts
@@ -2,7 +2,7 @@ import { Client, Account, ID, Databases, Storage } from 'appwrite';
 
 const client = new Client()
 	.setEndpoint('https://cloud.appwrite.io/v1')
-	.setProject(process.env.PROJECT_ID as string);
+	.setProject(process.env.NEXT_PUBLIC_PROJECT_ID!);
 
 const account = new Account(client);
 const databases = new Databases(client);
--- components/Board.tsx
+++ components/Board.tsx
@@ -1,7 +1,84 @@
-import { DragDropContext } from 'react-beautiful-dnd';
+'use client';
+
+import { useBoardStore } from '@/store/BoardStore';
+import { useEffect } from 'react';
+import { DragDropContext, DropResult, Droppable } from 'react-beautiful-dnd';
+import Column from './Column';
 
 function Board() {
-	return <div>Board</div>;
+	const { getBoard, board, setBoardState } = useBoardStore(state => state);
+
+	useEffect(() => {
+		getBoard();
+	}, [getBoard]);
+
+	const handleOnDragEnd = (result: DropResult) => {
+		const { destination, source, type } = result;
+
+		// check if user dragged card outside of board
+		if (!destination) return;
+
+		//handle column drag
+		if (type === 'column') {
+			const entries = Array.from(board.columns.entries());
+			const [removed] = entries.splice(source.index, 1);
+			entries.splice(destination.index, 0, removed);
+			const rearrangedColumns = new Map(entries);
+			setBoardState({
+				...board,
+				columns: rearrangedColumns,
+			});
+		}
+
+		const columns = Array.from(board.columns);
+		const startColIndex = columns[Number(source.droppableId)];
+		const finishColIndex = columns[Number(destination.droppableId)];
+
+		const startCol: Column = {
+			id: startColIndex[0],
+			todos: startColIndex[1].todos,
+		};
+
+		const finishCol: Column = {
+			id: finishColIndex[0],
+			todos: finishColIndex[1].todos,
+		};
+
+		if (!startCol || !finishCol) return;
+
+		if (source.index === destination.index && startCol === finishCol) return;
+
+		const newTodos = startCol.todos;
+		const [todomoved] = newTodos.splice(source.index, 1);
+
+		if (startCol.id === finishCol.id) {
+			newTodos.splice(destination.index, 0, todomoved);
+			const newCol = {
+				id: startCol.id,
+				todos: todomoved,
+			};
+
+			const newColumns = new Map(board.columns);
+			newColumns.set(startCol.id, newCol);
+		}
+	};
+
+	return (
+		<DragDropContext onDragEnd={handleOnDragEnd}>
+			<Droppable droppableId='board' direction='horizontal' type='column'>
+				{provided => (
+					<div
+						{...provided.droppableProps}
+						ref={provided.innerRef}
+						className='grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto'>
+						{Array.from(board.columns.entries()).map(([id, column], index) => (
+							<Column key={id} id={id} todos={column.todos} index={index} />
+						))}
+					</div>
+				)}
+			</Droppable>
+		</DragDropContext>
+	);
 }
 
 export default Board;
